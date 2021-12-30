8ee3bf11f2e08d7dcfb18ad13e74b49a18ef0236
Kaier33 <469439801@qq.com>
2021-12-30 18:07:11 +0800

    chore
    打包拆分

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,26 @@
 import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: './dist/tiny-monitor-sdk.js',
-      format: 'umd',
+      file: './dist/tiny-monitor-sdk.esm.js',
+      format: 'es',
+      name: 'TinyMonitorSdk'
+    },
+    {
+      file: './dist/tiny-monitor-sdk.min.js',
+      format: 'iife',
       name: 'TinyMonitorSdk'
     }
   ],
   plugins: [
-    // terser(),
     resolve(),
-    babel({ babelHelpers: 'bundled' })
+    commonjs({exclude: 'node_modules'}),
+    babel({ babelHelpers: 'bundled', exclude: 'node_modules/**' }),
+    terser()
   ]
 }
